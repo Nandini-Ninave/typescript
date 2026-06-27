77ba7c55fa074ce93781e801d9b70c22498b65cc
Nandini_Ninave <nandinininave99@gmail.com>
2026-06-27 12:56:26 +0530

    Compiled index.ts

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let message = "Hello, TypeScript!";
+let message = "Hello";
 console.log(typeof message);
-//# sourceMappingURL=index.js.map
+console.log(message);
